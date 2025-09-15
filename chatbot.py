27cba2dd3aae1358402416f54da59909b5562f75
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-15 17:35:38 +0530

    Stop tracking .env file

--- chatbot.py
+++ chatbot.py
@@ -3,13 +3,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatGroq(model="llama-3.3-70b-versatile",temperature=1)
-
+chat_history = []
 while True:
     user_input = input('you : ')
+    chat_history.append(user_input)
     if user_input=='exit':
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
     print ("AI: ",result.content)
+
+print(chat_history)
 
 
 
